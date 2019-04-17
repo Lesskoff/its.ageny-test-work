1185dbf63d4680b42535857de714d9293ddd68ff
haseri <iutas@bk.ru>
2019-04-17 16:02:26 +0300

    adding dist folder for deploying

---
+++ dist/precache-manifest.48dd5dc6ae80676dda80f494b646f230.js
@@ -0,0 +1,294 @@
+self.__precacheManifest = [
+  {
+    "revision": "69acfd1e0ec986e2cbf935ec3a93766e",
+    "url": "/fonts/TTNorms-Medium.69acfd1e.eot"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a99a1caa1dc8ffc3b0d5e54c9e15fa3b",
+    "url": "/fonts/TTNorms-Black.a99a1caa.eot"
+  },
+  {
+    "revision": "796b5b9f387a5444718d",
+    "url": "/css/chunk-vendors.804cf386.css"
+  },
+  {
+    "revision": "09500f3e284c40b28e0f879ef94aa9f4",
+    "url": "/img/shaverma.09500f3e.png"
+  },
+  {
+    "revision": "cf284fc1f8489def813e177904af3d00",
+    "url": "/img/steik.cf284fc1.png"
+  },
+  {
+    "revision": "1009f03324315eb8bb66d610775afe82",
+    "url": "/fonts/TTNorms-Bold.1009f033.woff"
+  },
+  {
+    "revision": "d523157ee5916706ecf9b68f057e7b0a",
+    "url": "/fonts/TTNorms-Thin.d523157e.woff"
+  },
+  {
+    "revision": "33f150c6ac81219f18b55864e087dd12",
+    "url": "/fonts/TTNorms-Regular.33f150c6.woff"
+  },
+  {
+    "revision": "942fa011bf304508216e82de00f5a868",
+    "url": "/fonts/TTNorms-Heavy.942fa011.woff"
+  },
+  {
+    "revision": "80dc0df7a146101811666e2b3a533e89",
+    "url": "/fonts/TTNorms-Light.80dc0df7.woff"
+  },
+  {
+    "revision": "004fb861db3f22b2677f37e95b598407",
+    "url": "/fonts/TTNorms-ExtraLight.004fb861.woff"
+  },
+  {
+    "revision": "98c072155c25195df9b0015da6dbe52f",
+    "url": "/fonts/TTNorms-Medium.98c07215.woff"
+  },
+  {
+    "revision": "5a246f88fc7f4539fa8d90711016d1c0",
+    "url": "/fonts/TTNorms-Black.5a246f88.woff"
+  },
+  {
+    "revision": "9a30e8f0dd95fceaa89cf08605ea7236",
+    "url": "/fonts/TTNorms-ExtraBoldItalic.9a30e8f0.woff"
+  },
+  {
+    "revision": "6ab9c43ba119ab72fe275d12c5345335",
+    "url": "/fonts/TTNorms-ThinItalic.6ab9c43b.woff"
+  },
+  {
+    "revision": "b073cdd300beb24e86302040f55b6658",
+    "url": "/fonts/TTNorms-Italic.b073cdd3.woff"
+  },
+  {
+    "revision": "00044896bccc73361ff754169377f201",
+    "url": "/fonts/TTNorms-BlackItalic.00044896.woff"
+  },
+  {
+    "revision": "a74a26160881dc1cfcc91adc6c8a0159",
+    "url": "/fonts/TTNorms-LightItalic.a74a2616.woff"
+  },
+  {
+    "revision": "a7cf3a0b88cb4a3c709c8570d2b8da5c",
+    "url": "/fonts/TTNorms-ExtraLightItalic.a7cf3a0b.woff"
+  },
+  {
+    "revision": "465218007e4961c4cfb7c69de65f48f1",
+    "url": "/fonts/TTNorms-BoldItalic.46521800.woff"
+  },
+  {
+    "revision": "bf2936348754a64d902b01fce87df43d",
+    "url": "/fonts/TTNorms-MediumItalic.bf293634.woff"
+  },
+  {
+    "revision": "968ac1564de430ec7daead4153e9ecca",
+    "url": "/fonts/TTNorms-HeavyItalic.968ac156.woff"
+  },
+  {
+    "revision": "faff73e68bc9c9a5ff7aae218f486e27",
+    "url": "/fonts/TTNorms-ExtraBold.faff73e6.woff"
+  },
+  {
+    "revision": "7695fb097d053793fabf690e505bdf00",
+    "url": "/fonts/TTNorms-Bold.7695fb09.eot"
+  },
+  {
+    "revision": "64f8b35b6c80f4b77fdc090d3cd9f616",
+    "url": "/fonts/TTNorms-Bold.64f8b35b.ttf"
+  },
+  {
+    "revision": "213cb930a634e1810bc5440f671c5220",
+    "url": "/fonts/TTNorms-Thin.213cb930.ttf"
+  },
+  {
+    "revision": "b5f6def0dda90a0e540a5fe011f63192",
+    "url": "/fonts/TTNorms-Thin.b5f6def0.eot"
+  },
+  {
+    "revision": "c585352cc2be6277de0cb0adab7cbede",
+    "url": "/fonts/TTNorms-Regular.c585352c.eot"
+  },
+  {
+    "revision": "eb6edc3979c854b8a687a8b973dda56a",
+    "url": "/fonts/TTNorms-Regular.eb6edc39.ttf"
+  },
+  {
+    "revision": "f9dca104f5e65b138bf3527bed5ea831",
+    "url": "/fonts/TTNorms-Heavy.f9dca104.eot"
+  },
+  {
+    "revision": "97ee7bc2191d6fd2092c53455389ab5f",
+    "url": "/fonts/TTNorms-Heavy.97ee7bc2.ttf"
+  },
+  {
+    "revision": "ca4fdb952f5c0d34179e41c06332d229",
+    "url": "/fonts/TTNorms-Light.ca4fdb95.eot"
+  },
+  {
+    "revision": "1504a2b537f15e92e32f9c9f6970057b",
+    "url": "/fonts/TTNorms-Light.1504a2b5.ttf"
+  },
+  {
+    "revision": "491f077bbf4e5baa8a03d2fda9dd614c",
+    "url": "/fonts/TTNorms-ExtraLight.491f077b.eot"
+  },
+  {
+    "revision": "368a68b67f5d4aac4c52ae479ea1cb90",
+    "url": "/fonts/TTNorms-ExtraLight.368a68b6.ttf"
+  },
+  {
+    "revision": "14b7997de9a880a14ec7",
+    "url": "/js/app.81703f14.js"
+  },
+  {
+    "revision": "796b5b9f387a5444718d",
+    "url": "/js/chunk-vendors.8ae8eb58.js"
+  },
+  {
+    "revision": "1082eb3bf7be09cb7eea9fd3c057732e",
+    "url": "/fonts/TTNorms-Medium.1082eb3b.ttf"
+  },
+  {
+    "revision": "3d7734f61bc614aa3cfcb607b124cbd0",
+    "url": "/fonts/TTNorms-MediumItalic.3d7734f6.eot"
+  },
+  {
+    "revision": "16f29f2c4a2f8ac9c05c4b7161c13f9b",
+    "url": "/fonts/TTNorms-HeavyItalic.16f29f2c.eot"
+  },
+  {
+    "revision": "5600c148b2f0c7fe3f0376de525a03a1",
+    "url": "/fonts/TTNorms-MediumItalic.5600c148.ttf"
+  },
+  {
+    "revision": "0570c4c971e771c5181fbb13ce9b926b",
+    "url": "/fonts/TTNorms-ExtraBold.0570c4c9.eot"
+  },
+  {
+    "revision": "64fd1cb0c26d604cfc3f71b384d30b97",
+    "url": "/fonts/TTNorms-ExtraBoldItalic.64fd1cb0.ttf"
+  },
+  {
+    "revision": "b60f40cea4fa7f81d95785e877c31645",
+    "url": "/fonts/TTNorms-Black.b60f40ce.ttf"
+  },
+  {
+    "revision": "5004356bed245579d153e5c7b48987f7",
+    "url": "/fonts/TTNorms-HeavyItalic.5004356b.ttf"
+  },
+  {
+    "revision": "46cc55b536b0f4c1f9da11c7b6d23eb6",
+    "url": "/fonts/TTNorms-ExtraBoldItalic.46cc55b5.eot"
+  },
+  {
+    "revision": "d87fbe2abcfcdc82fb44345ef7e4dfa7",
+    "url": "/fonts/TTNorms-ThinItalic.d87fbe2a.ttf"
+  },
+  {
+    "revision": "893b1e9258fa679d5689e3381eefb5ba",
+    "url": "/fonts/TTNorms-ThinItalic.893b1e92.eot"
+  },
+  {
+    "revision": "5e90ca92faad956c6201ba0a07a43d82",
+    "url": "/fonts/TTNorms-Italic.5e90ca92.ttf"
+  },
+  {
+    "revision": "13e12f04cba5cb33adafa6318b1194c3",
+    "url": "/fonts/TTNorms-Italic.13e12f04.eot"
+  },
+  {
+    "revision": "133e66efa5dd2e4b797144da1cf21fec",
+    "url": "/fonts/TTNorms-LightItalic.133e66ef.eot"
+  },
+  {
+    "revision": "2d2351a0c7b50c89f97dc14a1e3eac14",
+    "url": "/fonts/TTNorms-BlackItalic.2d2351a0.ttf"
+  },
+  {
+    "revision": "63aba23f4f0bf86edd9283f51c5f45a3",
+    "url": "/fonts/TTNorms-ExtraLightItalic.63aba23f.eot"
+  },
+  {
+    "revision": "ae73dc626a3cac6b6c2063b7f8efc64b",
+    "url": "/fonts/TTNorms-BoldItalic.ae73dc62.eot"
+  },
+  {
+    "revision": "12dc4f472cdce92a0fcfc9c67c2d8b3c",
+    "url": "/fonts/TTNorms-LightItalic.12dc4f47.ttf"
+  },
+  {
+    "revision": "441584c1a327027648040eca0a822f4d",
+    "url": "/fonts/TTNorms-ExtraLightItalic.441584c1.ttf"
+  },
+  {
+    "revision": "0d488549dd9652a3bd5afdbd4a8a49f0",
+    "url": "/fonts/TTNorms-BoldItalic.0d488549.ttf"
+  },
+  {
+    "revision": "cfcc44d4d2ae2bd956fd0c8f3be1c232",
+    "url": "/fonts/TTNorms-BlackItalic.cfcc44d4.eot"
+  },
+  {
+    "revision": "aaeea6a6c907fc502041418b37d5403f",
+    "url": "/fonts/TTNorms-ExtraBold.aaeea6a6.ttf"
+  },
+  {
+    "revision": "1ff757491ccb4cebebcdfb5cdff373a5",
+    "url": "/img/remove-basket.1ff75749.svg"
+  },
+  {
+    "revision": "9c6c8b4385dda68e50df352369e23efe",
+    "url": "/img/categories-btn.9c6c8b43.svg"
+  },
+  {
+    "revision": "e159e0480e1b77696eb47ae7ac1cd346",
+    "url": "/img/location.e159e048.svg"
+  },
+  {
+    "revision": "e3d70f5879d8a219497bb903d40a01a5",
+    "url": "/img/empty-cart.e3d70f58.svg"
+  },
+  {
+    "revision": "d935da77628084028ffb363b6a01cc3b",
+    "url": "/img/kovurma.d935da77.png"
+  },
+  {
+    "revision": "499ef16cc665f114c033569a9a287c1e",
+    "url": "/img/steik-large.499ef16c.png"
+  },
+  {
+    "revision": "0c8749c08c8caf3c53d44987080361d5",
+    "url": "/img/beshbarmak.0c8749c0.png"
+  },
+  {
+    "revision": "cb6bd3351a0a2f52aa3ad0b86fdae842",
+    "url": "/img/sudak.cb6bd335.png"
+  },
+  {
+    "revision": "86c4f1fcd6d72055e17d34d3db0453a1",
+    "url": "/img/plov-chaihanskiy.86c4f1fc.png"
+  },
+  {
+    "revision": "ab137c99e17e265f13038da6615dda76",
+    "url": "/img/shashlik.ab137c99.png"
+  },
+  {
+    "revision": "26763af69ae0155e592ddf0ff82ce173",
+    "url": "/img/manti.26763af6.png"
+  },
+  {
+    "revision": "48e15952a7085e5a344421525f70e3fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b7997de9a880a14ec7",
+    "url": "/css/app.549cd4fc.css"
+  }
+];
